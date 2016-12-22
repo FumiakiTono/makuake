a81dc7a213ea6c3c839c2d5bfe358f0a945f6dc7
FumiakiTono <tonotorious@gmail.com>
2016-12-22 19:04:27 +0900

    Create the routing about projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,24 @@
 class ProjectsController < ApplicationController
+
   def index
   end
+
+  def search
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def pay1
+  end
+
+  def pay2
+  end
+
+  def pay3
+  end
+
 end
