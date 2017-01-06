3159a65e4a4b341c01ed5eeafd2d4983e2c98d0c
FumiakiTono <tonotorious@gmail.com>
2017-01-06 14:16:33 +0900

    fix migration file

--- db/migrate/20161217045910_remove_column_from_user.rb
+++ db/migrate/20161217045910_remove_column_from_user.rb
@@ -1,6 +1,6 @@
 class RemoveColumnFromUser < ActiveRecord::Migration[5.0]
   def change
-    remove_column, :users, :email, :string
-    remove_column, :users, :password, :string
+    remove_column :users, :email, :string
+    remove_column :users, :password, :string
   end
 end
