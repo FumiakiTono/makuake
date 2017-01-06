2a69c19dc7642700be4ee79417cd88f70ecafc83
FumiakiTono <tonotorious@gmail.com>
2017-01-06 14:13:16 +0900

    delete unncessary code

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-ActiveRecord::Base.protected_environments = %w(staging)
-
-
 module Makuake
   class Application < Rails::Application
     config.generators do |g|
